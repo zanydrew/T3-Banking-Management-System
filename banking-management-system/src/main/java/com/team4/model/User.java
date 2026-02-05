822e7e7b2c1ce214bf4ddab22ad87a507b6933a8
zanydrew <nizahon00@gmail.com>
2026-02-05 12:29:40 +0700

    add User and Account java files

---
+++ banking-management-system/src/main/java/com/team4/model/User.java
@@ -0,0 +1,49 @@
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getDOB() {
+        return DOB;
+    }
+    public void setDOB(String dOB) {
+        DOB = dOB;
+    }public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getDOB() {
+        return DOB;
+    }
+    public void setDOB(String dOB) {
+        DOB = dOB;
+    }
+    
