4580a2daa33aa57f583c01307edcf0917b6b66e1
zanydrew <nizahon00@gmail.com>
2026-02-25 16:09:39 +0700

    implement Customer class

--- banking-management-system/src/main/java/com/team4/model/User.java
+++ banking-management-system/src/main/java/com/team4/model/User.java
@@ -1,5 +1,9 @@
 package com.team4.model;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Pattern;
+
 public abstract class User {
     private String id;
     private String username;
@@ -131,6 +135,92 @@ public abstract class User {
 
     protected void setRole(UserRole role) {
         this.role = role;
+    }
+
+    // ==== Helpers ===
+
+    public boolean isDigits(String s) {
+        if (s.isBlank())
+            return false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c < '0' || c > '9')
+                return false;
+        }
+        return true;
+    }
+
+    /*
+     * 1. check string length
+     * 2 check if it's digit
+     * 3. parse from month to month in number
+     * 4. check if:
+     * -dd between 01-31
+     * -mm between 01-12
+     * -yyyy <= current year
+     * -dd <=29 on feb
+     */
+
+    public boolean isValidDate(String date) {
+
+        String[] parts = date.split("-");
+        String dd = parts[0].trim();
+        String mm = parts[1].trim();
+        String yyyy = parts[2].trim();
+
+        LocalDate nowDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy");
+        String currentYear = nowDate.format(formatter);
+
+        if (dd.isBlank() || mm.isBlank() || yyyy.isBlank()
+                || !isDigits(dd) || !isDigits(mm) || !isDigits(yyyy)
+                || dd.length() != 2 || mm.length() != 2 || yyyy.length() != 4) {
+
+            System.out.println("\n" + "Date must enter in this format: dd-mm-yyyy.");
+            return false;
+        }
+
+        // check valid date
+        else if (Integer.parseInt(dd) > 31 || Integer.parseInt(dd) < 1
+                || Integer.parseInt(mm) > 12 || Integer.parseInt(mm) < 1
+                || Integer.parseInt(yyyy) > Integer.parseInt(currentYear)
+                || (Integer.parseInt(mm) == 2 && Integer.parseInt(dd) > 29)) {
+
+            System.out.println("\n" + "Please enter a accurate date (day: from 01 to 31, month: from 01 to 12).");
+            return false;
+        }
+
+        return true;
+    }
+
+    /*
+     * Password must be from 8-20 characters.
+     * must contain at lease 1 uppercase and lower case
+     * at least 1 speacial character
+     * no white space.
+     */
+
+    private boolean isValidPassword(String pw) {
+        final String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%&*_])[A-Za-z\\d!@#$%&*_]{8,20}$";
+        Pattern pattern = Pattern.compile(regex);
+
+        if (pw == null || !(pattern.matcher(pw).matches())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "Id='" + id + '\'' +
+                ", fullName='" + fullname + '\'' +
+                ", phone='" + phone + '\'' +
+                ", username='" + username + '\'' +
+                ", position='" + role + '\'' +
+                ", date of birth=" + dOB +
+                '}';
     }
 
 }
