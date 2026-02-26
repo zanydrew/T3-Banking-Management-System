a13592deb931e5e8eb1c17addc90205443ad1f21
zanydrew <nizahon00@gmail.com>
2026-02-26 11:23:21 +0700

    modify Account class

--- banking-management-system/src/main/java/com/team4/model/Account.java
+++ banking-management-system/src/main/java/com/team4/model/Account.java
@@ -1,54 +1,128 @@
 package com.team4.model;
 
+import java.util.regex.Pattern;
+
 public class Account {
-    private int accountID;
-    private String accountHolderName;
+    private String accountNumber;
+    private String holderName;
+    private String email;
+    private String accountPin;
+    private AccountType accountType;
+    private AccountStatus accountStatus;
     private double balance;
 
-    public Account(int acccountID, String accountHolderName, double balance) {
-        setAcountID(acccountID);
-        setAccountHolderName(accountHolderName);
+    // ==== Constructor ====
+    public Account(String accountNumber, String holderName, String email, String accountPin, AccountType accountType,
+            AccountStatus accountStatus,
+            double balance) {
+        setAccountNumber(accountNumber);
+        setHolderName(holderName);
+        setEmail(email);
+        setAccountPin(accountPin);
+        setAccountType(accountType);
+        setAccountStatus(accountStatus);
         setBalance(balance);
     }
 
-    // setter
-    public void setAcountID(int accountID) {
-        if (accountID <= 0) {
-            throw new IllegalArgumentException();
-        }
-        this.accountID = accountID;
+    // === Getters ===
+    public String getAccountNumber() {
+        return accountNumber;
     }
 
-    public void setAccountHolderName(String accountHolderName) {
-        if (accountHolderName.isBlank()) {
-            throw new IllegalArgumentException();
-        }
-        this.accountHolderName = accountHolderName;
+    public String getHolderName() {
+        return holderName;
     }
 
-    public void setBalance(double balance) {
-        if (balance < 0) {
-            throw new IllegalArgumentException();
-        }
-        this.balance = balance;
+    public String getEmail() {
+        return email;
     }
 
-    // Getter
-    public int getAccountID() {
-        return accountID;
+    public String getAccountPin() {
+        return accountPin;
     }
 
-    public String getAccountHoldername() {
-        return accountHolderName;
+    public AccountType getAccountType() {
+        return accountType;
     }
 
     public double getBalance() {
         return balance;
     }
 
-    @Override
-    public String toString() {
-        return "Account ID: " + accountID + " | Name: " + accountHolderName + " | Balance: $" + balance;
+    public AccountStatus getAccountStatus() {
+        return accountStatus;
     }
 
+    // === Setters ===
+    protected void setAccountStatus(AccountStatus accountStatus) {
+        this.accountStatus = accountStatus;
+    }
+
+    protected void setAccountNumber(String accountNumber) {
+        if (accountNumber.isEmpty() || accountNumber.isBlank()) {
+            throw new IllegalArgumentException("Account must have account number.");
+        } else if (!isDigits(accountNumber) || accountNumber.length() > 12) {
+            throw new IllegalArgumentException("Invalid account number");
+        }
+        this.accountNumber = accountNumber.trim();
+    }
+
+    // did not take full name because 1 account could have multiple holders
+    protected void setHolderName(String holderName) {
+        if (holderName.isEmpty() || holderName.isBlank()) {
+            throw new IllegalArgumentException("Full name must be filled.");
+        }
+        this.holderName = holderName.trim();
+    }
+
+    protected void setEmail(String email) {
+        if (email.isEmpty() || email.isBlank()) {
+            this.email = "No Email";
+        } else if (!(isValidEmail(email))) {
+            throw new IllegalArgumentException(
+                    "Email must be valid");
+        }
+        this.email = email.trim();
+    }
+
+    protected void setAccountPin(String accountPin) {
+        if (accountPin.isEmpty() || accountPin.isBlank()) {
+            throw new IllegalArgumentException("Pin must be filled.");
+        } else if (!isDigits(accountPin) || accountPin.length() != 6) {
+            throw new IllegalArgumentException("Pin must have 6 Numbers");
+        }
+        this.accountPin = accountPin;
+    }
+
+    protected void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+
+    protected void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // === Helpers ===
+
+    public boolean isValidEmail(String e) {
+        final String email_regex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@"
+                + "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(email_regex);
+
+        if (e == null || !(pattern.matcher(e).matches())) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isDigits(String s) {
+        if (s.isBlank())
+            return false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c < '0' || c > '9')
+                return false;
+        }
+        return true;
+    }
 }
---
+++ banking-management-system/src/main/java/com/team4/model/AccountStatus.java
@@ -0,0 +1,5 @@
+package com.team4.model;
+
+public enum AccountStatus {
+    ACTIVE, CLOSE, BLOCK;
+}
