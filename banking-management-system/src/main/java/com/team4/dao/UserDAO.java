04cacdce7e1b5d0039ca372715a067950bcf70c3
zanydrew <nizahon00@gmail.com>
2026-03-23 19:48:59 +0700

    create UserDAO interface

---
+++ banking-management-system/src/main/java/com/team4/dao/UserDAO.java
@@ -0,0 +1,15 @@
+package com.team4.dao;
+
+import com.team4.model.user.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface UserDAO {
+    User findUserById(int userId) throws SQLException;
+    User findUserByUsername(String username) throws SQLException;
+    List<User> findAllUsers() throws SQLException;
+    void updateUserPhone(int userId, String newPhone) throws SQLException;
+    void updateUserFullName(int userId, String newFullName) throws SQLException;
+    void deleteUser(int userId) throws SQLException;         // manager only
+}
---
+++ banking-management-system/src/main/java/com/team4/dao/impl/MySQLTransactionDAO.java
@@ -0,0 +1,4 @@
+package com.team4.dao.impl;
+
+public class MySQLTransactionDAO {
+}
