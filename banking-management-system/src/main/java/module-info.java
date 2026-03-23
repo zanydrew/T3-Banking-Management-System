36f7d2bba307b1f2b09a664264b449a2edaefe71
zanydrew <nizahon00@gmail.com>
2026-03-23 19:04:19 +0700

    implement DatabaseConnection

--- banking-management-system/src/main/java/module-info.java
+++ banking-management-system/src/main/java/module-info.java
@@ -1,7 +1,16 @@
 module com.team4 {
+    // JavaFX
     requires javafx.controls;
     requires javafx.fxml;
 
+    // Database
+    requires java.sql;
+    requires mysql.connector.j;     // or mysql.connector.java
+
+
+    // Make controllers accessible to JavaFX
     opens com.team4 to javafx.fxml;
+    opens com.team4.controller to javafx.fxml;
+
     exports com.team4;
 }
