a00558c49e4ad17f54a06ef77c6bcb9a4ffe8450
zanydrew <nizahon00@gmail.com>
2026-03-26 20:36:03 +0700

    test + fix module-info

--- banking-management-system/src/main/java/module-info.java
+++ banking-management-system/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.team4 {
     // JavaFX
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.base;
 
     // Database
     requires java.sql;
@@ -11,6 +12,12 @@ module com.team4 {
     // Make controllers accessible to JavaFX
     opens com.team4 to javafx.fxml;
     opens com.team4.controller to javafx.fxml;
+
+    // Allow JavaFX reflection access to ALL model packages
+    opens com.team4.model.transaction to javafx.base, javafx.controls;
+    opens com.team4.model.account     to javafx.base, javafx.controls;
+    opens com.team4.model.user        to javafx.base, javafx.controls;
+    opens com.team4.session  to javafx.base, javafx.controls;
 
     exports com.team4;
     exports com.team4.controller;
