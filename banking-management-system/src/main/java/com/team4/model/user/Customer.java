cd560a8406439b9218455fa0044235393c8b2390
zanydrew <nizahon00@gmail.com>
2026-03-27 21:16:16 +0700

    modify Customer class for database friendly

--- banking-management-system/src/main/java/com/team4/model/user/Customer.java
+++ banking-management-system/src/main/java/com/team4/model/user/Customer.java
@@ -1,8 +1,22 @@
 package com.team4.model.user;
 
 public class Customer extends User {
-    public Customer(int userId, String username, String password, String fullname, String phone, String dOB,
-            UserRole role) {
-        super(userId, username, password, fullname, phone, dOB, UserRole.CUSTOMER);
+    // Used for NEW customer creation (validated)
+    public Customer(int id, String username, String password,
+                    String fullname, String phone, String dOB, UserRole customer) {
+        super(id, username, password, fullname, phone, dOB, UserRole.CUSTOMER);
+    }
+
+    // Used for DB (trusted data, no validation)
+    public Customer(int id, String username, String password,
+                    String fullname, String phone, String dOB, boolean trusted) {
+        super(id, username, password, fullname, phone, dOB, UserRole.CUSTOMER, trusted);
+    }
+
+    @Override
+    public String toString() {
+        return "User ID: " + getUserId() + " \nusername: " + getUsername() + "\nFull Name: "
+                + getFullname() + "\nPhone Number: " + getPhone() + "\nDate of Birth: " + getdOB() + "\nUser Role: "
+                + getRole();
     }
 }
