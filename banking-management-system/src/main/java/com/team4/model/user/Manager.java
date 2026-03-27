31fe504fd3b783808f8941c8172ce08ad6eb5125
zanydrew <nizahon00@gmail.com>
2026-03-27 21:16:55 +0700

    modify Manager class for database friendly

--- banking-management-system/src/main/java/com/team4/model/user/Manager.java
+++ banking-management-system/src/main/java/com/team4/model/user/Manager.java
@@ -2,8 +2,20 @@ package com.team4.model.user;
 
 public class Manager extends User {
 
-    public Manager(int userId, String username, String password, String fullname, String phone, String dOB,
-            UserRole role) {
-        super(userId, username, password, fullname, phone, dOB, UserRole.MANAGER);
+    public Manager(int id, String username, String password,
+                   String fullname, String phone, String dOB) {
+        super(id, username, password, fullname, phone, dOB, UserRole.MANAGER);
+    }
+
+    public Manager(int id, String username, String password,
+                   String fullname, String phone, String dOB, boolean trusted) {
+        super(id, username, password, fullname, phone, dOB, UserRole.MANAGER, trusted);
+    }
+
+    @Override
+    public String toString() {
+        return "User ID: " + getUserId() + " \nusername: " + getUsername() + "\nFull Name: "
+                + getFullname() + "\nPhone Number: " + getPhone() + "\nDate of Birth: " + getdOB() + "\nUser Role: "
+                + getRole();
     }
 }
